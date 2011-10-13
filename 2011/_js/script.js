09af9e77319a0605531a68adf091e91fdbbfdb2f
Prateek Rungta <prateekrungta@gmail.com>
2011-10-13 16:20:46 +0530

    Parallax banner for widescreen browsers

--- 2011/_js/script.js
+++ 2011/_js/script.js
@@ -1,6 +1,13 @@
 function main () {
 	$('#header .titles h1').fitText(0.274);
 	$('#header .titles h2').fitText(1.005);
+	
+	parallax();
+}
+
+function parallax () {
+	$('#parallax li').plaxify();
+	$.plax.enable();
 }
 
 $(main);
