b85e6ec69c2bae46788f4f60a4bbb01ccedf6a4d
Vidya Ramakrishnan <vidya@hasgeek.in>
2015-12-22 10:22:50 +0530

    Closing droidconIN 2015.
    1. Remove Buy Ticket buttons.
    2. Remove ticket and schedule sections.
    3. Update annoucement section.
    4. Add credit section.
    5. Link to event photos.

--- static/js/main.js
+++ static/js/main.js
@@ -445,7 +445,7 @@ $(document).ready(function() {
             $('.subscribe-status').html('Please enter an email id');
         }
         else {
-            postData = { "Email": $('#subscribe-email').val(), "Event" : "DroidCon India 2015" };
+            postData = { "Email": $('#subscribe-email').val(), "Event" : "DroidCon India 2016" };
             $('.ajax-loader').css('visibility', 'visible');
             $.ajax({
                 type: 'post',
